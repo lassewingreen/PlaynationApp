641390566772ae27ec6bd546d1017efc3a08162e
Remus Cicu <RemusAndrei@remuss-mbp.home>
2013-12-12 21:03:53 +0100

    First commit

---
+++ Playnation App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Playnation App
+//
+//  Created by Remus Cicu on 12/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Playnation AppTests/Playnation_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Playnation_AppTests.m
+//  Playnation AppTests
+//
+//  Created by Remus Cicu on 12/12/13.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Playnation_AppTests : XCTestCase
+
+@end
+
+@implementation Playnation_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
